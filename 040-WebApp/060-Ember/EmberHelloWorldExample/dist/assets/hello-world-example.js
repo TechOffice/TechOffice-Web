b1bb36063e758ce481613693e60d30de4b4c2609
imben1109 <Ben_c@ITD-PC2220.mpfahk.org>
2016-12-30 17:59:04 +0800

    update

--- 040-WebApp/060-Ember/EmberHelloWorldExample/dist/assets/hello-world-example.js
+++ 040-WebApp/060-Ember/EmberHelloWorldExample/dist/assets/hello-world-example.js
@@ -319,7 +319,7 @@ catch(err) {
 /* jshint ignore:start */
 
 if (!runningTests) {
-  require("hello-world-example/app")["default"].create({"name":"hello-world-example","version":"0.0.0+bde82a5e"});
+  require("hello-world-example/app")["default"].create({"name":"hello-world-example","version":"0.0.0+a5d3cbbe"});
 }
 
 /* jshint ignore:end */
