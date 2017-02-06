a9daa0d1177c9e54f1d39a465c296b2e8e88802d
imben1109 <Ben_c@ITD-PC2220.mpfahk.org>
2017-02-06 17:12:36 +0800

    update

--- 999-Dev/SimpleCsvConvertor/src/CsvConvertor.js
+++ 999-Dev/SimpleCsvConvertor/src/CsvConvertor.js
@@ -3,8 +3,15 @@
  * (Javascript Module Pattern)
  */
 csvConvertor = (function () {
+	
+  var csvTableId = "csvTable";
+  
   return {
 
+	getCsvTableId: function(){
+		return csvTableId;
+	},
+  
 	/**
 	* getTableMeta is to get table meta data 
 	* such as the number of column and number of row
@@ -36,7 +43,7 @@ csvConvertor = (function () {
     convertCsvToTable: function (csv) {
       var meta = this.getTableMeta(csv);
       var tableContent = "";
-      tableContent = tableContent + "<table>";
+      tableContent = tableContent + "<table id='"+ this.getCsvTableId() +"'>";
       for (var i = 0; i < csv.length; i++) {
         tableContent = tableContent + "<tr>";
         var cols = csv[i];
@@ -60,6 +67,60 @@ csvConvertor = (function () {
       }
       tableContent = tableContent + "</table>";
       return tableContent;
-    }
+    },
+	
+	/**
+	* This method is get Json Data from the generated table 
+	* (The generated table would be altered which make it differs from the orginally imported csv file.)
+	*/
+	getJsonFromTable: function(){
+		var jsonValue = {};
+		var colNum = 0;
+		var rowNum = 0;
+		var csvTable = document.getElementById(this.getCsvTableId());
+		if (csvTable){
+			var rows = csvTable.querySelectorAll("tr");
+			rowNum = rows.length;
+			var csvData = [];
+			for (var i=0; i<rows.length; i++){
+				var row = rows[i];
+				var rowData = [];
+				var cols = row.querySelectorAll("td");
+				if (cols.length > colNum){
+					colNum = cols.length ;
+				}
+				for (var j=0; j<cols.length; j++){
+					var col = cols[j];
+					var colHtml = col.innerHTML;
+					while(colHtml.indexOf("&nbsp;") > -1 ){
+						colHtml = colHtml.replace("&nbsp;", "");
+					}
+					rowData.push(colHtml);
+				}
+				csvData.push(rowData);
+			}
+			jsonValue.csv = csvData;
+		}
+		jsonValue.colNum = colNum;
+		jsonValue.rowNum = rowNum;
+		return jsonValue;
+	},
+	
+	/**
+	* Convert the csvTable to csv content
+	*/
+	covertTableToCsv: function(){
+		var csvJson = this.getJsonFromTable();
+		var rows = csvJson.csv;
+		var csvStr = "";
+		var tempRowArr = [];
+		for (var i=0; i<rows.length; i++){
+			var row = rows[i];
+			var rowStr = row.join(',');
+			tempRowArr.push(rowStr);
+		}
+		csvStr = tempRowArr.join('\r\n');
+		return csvStr;
+	}
   };
 })();
