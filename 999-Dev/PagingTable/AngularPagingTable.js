c0a552d0e4fcce18b0d0d3084fa4b78832a1fa93
imben1109 <Ben_c@mpfahk.org>
2017-03-21 14:44:40 +0800

    update

--- 999-Dev/PagingTable/AngularPagingTable.js
+++ 999-Dev/PagingTable/AngularPagingTable.js
@@ -1,46 +1,33 @@
-var testApp = angular.module('testApp', []);
-// define TestController on phonecatApp
-testApp.controller('TestController', function TestController($scope) {
-	$scope.name = "Test Two-way Data Binding";
-	$scope.queryStr = "";
-	$scope.data = [
-		{
-			'name' : 'name 1',
-			'value' : 'value 1'
-		}, 
-		{
-			'name' : 'name 2',
-			'value' : 'value 2'
-		}, 
-		{
-			'name' : 'name 3',
-			'value' : 'value 3'
-		}, 
-		{
-			'name' : 'name 33',
-			'value' : 'value 33'
-		}, 
-		{
-			'name' : 'name 4',
-			'value' : 'value 4'
-		}, 
-		{
-			'name' : 'name 5',
-			'value' : 'value 5'
-		}
-	];
-	$scope.filteredData = $scope.data;
-	$scope.query = function(){
-		if (this.queryStr != ''){
-			this.filteredData = [];
-			for (var i=0; i<this.data.length; i++){
-				var item = this.data[i];
-				if (item.name.includes(this.queryStr)){
-					this.filteredData.push(item);
-				}
+angular.module('testApp', []).component('angularTable', {
+	templateUrl: "AngularPagingTableTemplate.html",
+	controller: function($scope){
+		$scope.labels = ['Name', 'Value'],
+		$scope.columns = ['name', 'value'],
+		$scope.data = [
+			{
+				'name' : 'name 1',
+				'value' : 'value 1'
+			}, 
+			{
+				'name' : 'name 2',
+				'value' : 'value 2'
+			}, 
+			{
+				'name' : 'name 3',
+				'value' : 'value 3'
+			}, 
+			{
+				'name' : 'name 33',
+				'value' : 'value 33'
+			}, 
+			{
+				'name' : 'name 4',
+				'value' : 'value 4'
+			}, 
+			{
+				'name' : 'name 5',
+				'value' : 'value 5'
 			}
-		}else{
-			this.filteredData = this.data;
-		}
+		];
 	}
 });
